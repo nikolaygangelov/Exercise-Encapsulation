a8167d41f4f1cbf5ff5359f236373f5d69d8024c
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-03-07 23:57:54 +0200

    Add files via upload

---
+++ Class Box Data/Box.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double lenght, double width, double height)
+        {
+            Length = lenght;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        {
+            get { return length ; }
+            private set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Length cannot be zero or negative.");
+                }
+                length  = value;
+            }
+        }
+
+
+        public double Width
+        {
+            get { return width; }
+            private set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Width cannot be zero or negative.");
+                }
+                width = value; 
+            }
+        }
+
+
+        public double Height
+        {
+            get { return height; }
+            private set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Height cannot be zero or negative.");
+                }
+                height = value; 
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            return 2 * (Length * Width + Length * Height + Width * Height);
+        }
+
+        public double LateralSurfaceArea()
+        {
+            return 2 *(Length * Height + Width * Height);
+        }
+
+        public double Volume()
+        {
+            return Length * Width * Height;
+        }
+
+    }
+}
